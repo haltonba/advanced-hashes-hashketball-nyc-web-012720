1459fbcf3deadfbf636b92ab2738e57bde17de2d
Halton Bagley <haltonba94@gmail.com>
2020-01-13 19:49:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -49,7 +49,7 @@ def game_hash
           slam_dunks: 5
         },
         {
-          player_name:,
+          player_name: "Jason Terry",
           number:,
           shoe:,
           points:,
