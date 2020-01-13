b51acd4383bd9d75cb79c0f5efcd9dcea0643b8d
Halton Bagley <haltonba94@gmail.com>
2020-01-13 19:41:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,12 +3,16 @@ def game_hash
     home: {
       team_name: "Brooklyn Nets",
       colors: ["Black", "White"],
-      players:
+      players: [
+        
+        ]
     },
     away: {
       team_name: "Charlotte Hornets",
       colors: ["Turquoise", "Purple"],
-      players:
+      players: [
+        
+        ]
     }
   }
 end
